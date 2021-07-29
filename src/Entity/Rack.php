3942cf6da033b79b28e769cd528f97269b85070c
Victor <victor30.pernot@gmail.com>
2021-07-29 10:42:00 +0200

    repare rack

--- src/Entity/Rack.php
+++ src/Entity/Rack.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\RackRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
@@ -44,4 +46,6 @@ class Rack
 
         return $this;
     }
+
+   
 }
