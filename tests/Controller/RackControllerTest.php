0b6ff0c6e93302eda7ca094b3b42884bfead5458
Victor <victor30.pernot@gmail.com>
2021-07-29 11:45:14 +0200

    test rack

---
+++ tests/Controller/RackControllerTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class RacksControllerTest extends WebTestCase
+{
+    public function testCreate(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/racks/');
+
+        $this->assertResponseIsSuccessful();
+        $client->clickLink('Create new');
+        $client->submitForm('Save', [
+            'rack[name]' => 'toto',
+        ]);
+        $this->assertResponseRedirects('/racks/');
+    }
+
+    /**
+     * @dataProvider provideInvalidInput
+     */
+    public function testCreateWithInvalidInput(string $name, string $expected): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/racks/');
+
+        $this->assertResponseIsSuccessful();
+        $client->clickLink('Create new');
+        $client->submitForm('Save', [
+            'rack[name]' => $name,
+        ]);
+        $this->assertSelectorTextContains('li', $expected);
+    }
+
+    public function provideInvalidInput(): array
+    {
+        return [
+            'empty' => ['', 'This value should not be blank.'],
+            'toolong' => [str_repeat('a', 256), 'This value is too long. It should have 255 characters or less.'],
+        ];
+    }
+}
